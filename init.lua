a3fed0a0dee34b28c17e20199f0bdb0b75c31a81
Jan Felix Langenbach <insert-penguin@protonmail.com>
2022-07-21 20:05:52 +0200

    Clean up init.lua

--- init.lua
+++ init.lua
@@ -1,22 +1,16 @@
 -- init.lua
 
-local utils = require("utils")
-local Augroup = require("utils.Augroup")
-local bind = require("utils.bind")
-local conf = require("utils.conf")
-local package = require("utils.package")
-local path = require("utils.path")
-
-local confdir = conf.dir
-local loadconf = conf.load
-local defined = utils.predicate(vim.fn.exists)
+local loadconf = require("utils.conf").load
 
 loadconf "basic"
 loadconf "filetypes"
 
-if defined "g:vscode" then loadconf "vscode" end
+if vim.g.vscode then
+	-- VSCode specific configuration.
+	-- See: https://github.com/vscode-neovim/vscode-neovim
+end
 
-if defined "g:neovide" then
+if vim.g.neovide then
 	vim.g.neovide_cursor_animation_length = 0.03
 	vim.g.neovide_remember_window_size = false
 	vim.g.neovide_remember_window_position = false
